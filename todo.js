86f8f6d582cf0db894ea994e2ea14b8c2325f1e9
wlakernotalnanathan <25@felix-au.me>
2026-08-02 04:10:43 +0530

    Update todo.js - Step 10

--- todo.js
+++ todo.js
@@ -2,3 +2,6 @@ const list = ['Code', 'Test', 'Ship'];
 const ul = document.getElementById('tasks');
 if(ul) { list.forEach(t => {
     let li = document.createElement('li');
+    li.innerText = t;
+    ul.appendChild(li);
+});}
